7004d66910d01e03d2213e9d16aace9e4449405f
Jérémie Bolduc <jeremiebolducjs@gmail.com>
2025-10-19 11:17:10 -0400

    added Chain function

--- guard.go
+++ guard.go
@@ -9,5 +9,23 @@ type Guard interface {
 type GuardFunc func(ctx context.Context, fn func(ctx context.Context) (any, error)) (any, error)
 
 func (gf GuardFunc) Do(ctx context.Context, fn func(ctx context.Context) (any, error)) (any, error) {
-    return gf(ctx, fn)
+	return gf(ctx, fn)
+}
+
+// Chain wraps multiple guards together into a single one.
+// The first argument becomes the outermost guard, and the last argument
+// becomes the innermost. For example, Chain(A, B, C) produces a policy
+// that executes in the order: A -> B -> C
+func Chain(guards ...Guard) Guard {
+	return GuardFunc(func(ctx context.Context, fn func(ctx context.Context) (any, error)) (any, error) {
+		wrapped := fn
+		for i := len(guards) - 1; i >= 0; i-- {
+			p := guards[i]
+			inner := wrapped
+			wrapped = func(ctx context.Context) (any, error) {
+				return p.Do(ctx, inner)
+			}
+		}
+		return wrapped(ctx)
+	})
 }
